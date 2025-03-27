43cd761995c2cf8f84412dcadaa831f0f303a5fc
Stephanie Adriane <stephanieadriane70@gmail.com>
2025-03-27 14:43:44 -0300

    Update landmarks.ts

--- src/data/landmarks.ts
+++ src/data/landmarks.ts
@@ -43,7 +43,7 @@ export const landmarks = [
   },
   {
     id: 7,
-    name: "t the RedeemChriser",
+    name: "Christ the Redeemer",
     location: "Rio de Janeiro, Brazil",
     image: "https://images.unsplash.com/photo-1705937565467-d073fe86e8f4?q=80&w=1548&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     hint: "Iconic statue overlooking the city from Corcovado Mountain"
